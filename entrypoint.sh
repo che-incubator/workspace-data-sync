8c82c4d5044cdd2d11f9fb9513f52b9a7665a8ac
Vitalii Parfonov <vparfono@redhat.com>
2020-04-08 15:33:19 +0300

    Add license header
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+#
+# Copyright (c) 2019-2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
 
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
